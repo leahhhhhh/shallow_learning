18e409199a60633b0111c59452103aab9a7e0179
jw <ljw1271005234@163.com>
2023-12-14 01:29:05 +1100

    vit_files

---
+++ cifar_dataloader.py
@@ -0,0 +1,70 @@
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+
+class DatasetArray(Dataset):
+    def __init__(self,features,labels,transform):
+        self.features=features
+        self.labels=labels
+        self.transform=transform
+    def __len__(self):
+        return len(self.labels)
+    def __getitem__(self,index):
+        features=self.features[index]
+        labels=self.labels[index]
+        #features = Image.fromarray(features.astype(np.uint8))
+        features_np = features.numpy()
+
+        # Convert NumPy array to PIL Image
+        features= Image.fromarray((features_np * 255).astype('uint8').transpose(1, 2, 0))
+        
+        if self.transform is not None:
+            features=self.transform(features)
+            #print(features.shape)
+        return (features,labels)
+    
+
+def dataloader1(dataset):
+    train_features=dataset['Xtr']
+    train_labels=dataset['Str']
+    train_labels = torch.from_numpy(train_labels)
+    train_labels=torch.nn.functional.one_hot(train_labels, num_classes=3).float()
+    test_features=dataset['Xts']
+    test_labels=dataset['Yts']
+    test_labels = torch.from_numpy(test_labels)
+    test_labels=torch.nn.functional.one_hot(test_labels, num_classes=3).float()
+    x_train,x_val,y_train,y_val=train_test_split(train_features,train_labels,test_size=0.2)
+    x_train=torch.from_numpy(x_train.astype(np.float32) / 255).permute(0, 3, 1, 2)
+    x_val=torch.from_numpy(x_val.astype(np.float32) / 255).permute(0, 3, 1, 2)
+    x_test=torch.from_numpy(test_features.astype(np.float32) / 255).permute(0, 3, 1, 2)
+    #batch load
+    data_transform = {
+    "train": transforms.Compose([
+                                    transforms.Resize(256),
+                                    transforms.CenterCrop(224),
+                                    #transforms.RandomResizedCrop(224),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]),
+    "val": transforms.Compose([transforms.Resize(256),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor(),
+                                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]),
+    "test":transforms.Compose([transforms.ToTensor()])}
+    train_dataset=DatasetArray(x_train,y_train,transform=data_transform["train"])
+    val_dataset=DatasetArray(x_val,y_val,transform=data_transform["val"])
+    # train_dataset=DatasetArray(x_train,y_train)
+    # val_dataset=DatasetArray(x_val,y_val)
+    test_data=DatasetArray(x_test,test_labels,transform=data_transform["test"])
+    
+    train_loader=DataLoader(train_dataset,batch_size=64,shuffle=True)
+    val_loader=DataLoader(val_dataset,batch_size=64,shuffle=True)
+    test_loader=DataLoader(test_data,batch_size=64,shuffle=True)
+
+    return train_loader,val_loader,test_loader
+
---
+++ train_evaluate.py
@@ -0,0 +1,76 @@
+import numpy as np
+import seaborn as sns
+import torch
+from sklearn.metrics import precision_recall_fscore_support
+
+sns.set()
+
+def train(model, criterion, optimizer, training_dataloader):
+    sum_accuracy = 0
+    sum_loss = 0
+    count = 0
+    model.train()
+    for features, labels in training_dataloader:
+        probabilities=model(features)
+        loss=criterion(probabilities,labels)
+        #print(loss.item())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        mini_batch_size=labels.size(0)
+        labels=torch.argmax(labels, dim=1)
+        accuracy=(
+            torch.sum(torch.max(probabilities,dim=1).indices==labels)
+        )
+
+        sum_accuracy+=accuracy.item()
+        sum_loss+=loss.item()*mini_batch_size
+        #print(sum_loss)
+        count+=mini_batch_size
+        #print(sum_loss/count)
+        train_acc=sum_accuracy/count
+        train_loss=sum_loss/count
+    return train_acc, train_loss
+
+
+def validate(model,criterion,dataloader):
+    sum_accuracy=0
+    sum_loss=0
+    count=0
+    model.eval()
+    for features ,labels in dataloader:
+        
+        with torch.no_grad():
+            probabilities=model(features)
+            loss=criterion(probabilities,labels)
+        mini_batch_size=labels.size(0)
+        labels=torch.argmax(labels, dim=1)
+        accuracy=(
+            torch.sum(torch.max(probabilities,dim=1).indices==labels)
+        )
+
+        sum_accuracy+=accuracy.item()
+        sum_loss+=loss.item()*mini_batch_size
+        #print(sum_loss)
+        count+=mini_batch_size
+        #print(sum_loss/count)
+        val_acc=sum_accuracy/count
+        val_loss=sum_loss/count
+    return val_acc, val_loss
+    
+def evaluate(dataset,model):
+    x_test=dataset["Xts"]
+    y_test=dataset["Yts"]
+    x_test=torch.from_numpy(x_test.astype(np.float32) / 255).permute(0, 3, 1, 2)
+    cifar_xtest=torch.Tensor(x_test)
+    y_pred=torch.max(model(cifar_xtest),dim=1).indices
+    correct=0
+    for index , value in enumerate(y_pred):
+        if value == y_test[index]:
+            correct += 1
+    #report=classification_report(y_test,y_pred.numpy())
+    precision,recall,_,_=precision_recall_fscore_support(y_test,y_pred,average='weighted')
+    return precision, recall
+
+
+        
