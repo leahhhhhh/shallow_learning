e5478f91e6bfc04c520ed568e0cd1d34539a71a9
jw <ljw1271005234@163.com>
2023-12-16 01:10:37 +1100

    train_evaluate_v2

--- train_evaluate.py
+++ train_evaluate.py
@@ -2,6 +2,7 @@ import numpy as np
 import seaborn as sns
 import torch
 from sklearn.metrics import precision_recall_fscore_support
+import torchvision.transforms as transforms
 
 sns.set()
 
@@ -11,6 +12,8 @@ def train(model, criterion, optimizer, training_dataloader):
     count = 0
     model.train()
     for features, labels in training_dataloader:
+        #labels = torch.argmax(labels, dim=1)
+        print('train_shape',labels.shape)
         probabilities=model(features)
         loss=criterion(probabilities,labels)
         #print(loss.item())
@@ -59,18 +62,43 @@ def validate(model,criterion,dataloader):
     return val_acc, val_loss
     
 def evaluate(dataset,model):
-    x_test=dataset["Xts"]
-    y_test=dataset["Yts"]
-    x_test=torch.from_numpy(x_test.astype(np.float32) / 255).permute(0, 3, 1, 2)
-    cifar_xtest=torch.Tensor(x_test)
-    y_pred=torch.max(model(cifar_xtest),dim=1).indices
+    # x_test=dataset["Xts"]
+    # y_test=dataset["Yts"]
+    # x_test=torch.from_numpy(x_test.astype(np.float32) / 255).permute(0, 3, 1, 2)
+    # cifar_xtest=torch.Tensor(x_test)
+    # print(cifar_xtest.shape)
+    # y_pred=torch.max(model(cifar_xtest),dim=1).indices
+    # correct=0
+    # for index , value in enumerate(y_pred):
+    #     if value == y_test[index]:
+    #         correct += 1
+    # #report=classification_report(y_test,y_pred.numpy())
+    # precision,recall,_,_=precision_recall_fscore_support(y_test,y_pred,average='weighted')
+
     correct=0
-    for index , value in enumerate(y_pred):
-        if value == y_test[index]:
-            correct += 1
-    #report=classification_report(y_test,y_pred.numpy())
-    precision,recall,_,_=precision_recall_fscore_support(y_test,y_pred,average='weighted')
-    return precision, recall
+    sum_count=0
+    precision_all=0
+    recall_all=0
+    times=0
+    model.eval()
+    for features , labels in dataset:
+        with torch.no_grad():
+            probabilities=model(features)
+            y_pred=torch.max(probabilities,dim=1).indices
+            for index , value in enumerate(y_pred):
+                if value == labels[index]:
+                    correct += 1
+            sum_count+=len(y_pred)
+            precision,recall,_,_=precision_recall_fscore_support(labels,y_pred,average='weighted')
+            precision_all+=precision
+            recall_all+=recall
+            times+=1
+    
+    accuracy=correct/sum_count
+    precision=precision_all/times
+    recall=recall_all/times
+
+    return precision, recall,accuracy
 
 
         
